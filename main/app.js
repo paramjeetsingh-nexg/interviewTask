074e1233cc3c2c279e8fe353eb9c778caea9e7dd
paramjeetsingh <paramjeet@nexgsolution.com>
2022-08-23 21:56:48 +0530

    add fake data function

--- main/app.js
+++ main/app.js
@@ -3,6 +3,9 @@ const app = express();
 require("./connection");
 const colors = require("colors");
 const item = require("./schema");
+const faker= require("faker");
+const carName = require('car-names')
+const { v4: uuidv4 } = require("uuid");
 
 const PORT = process.env.PORT || 8000;
 
@@ -14,10 +17,10 @@ app.get("/hello", (req, res) => {
 
 app.get("/", async (req, res) => {
   try {
-    const data = await item.find();
-    console.log("Data: ".black.bold, data);
+    const data = await item.find().select('-_id');
     res.status(200).send({
       success: true,
+      count: data.length,
       Data: data,
     });
   } catch (e) {
@@ -40,12 +43,44 @@ app.post("/item", async (req, res) => {
       });
     }
 
-    const Itemdata = new item(req.body);
-    console.log("Daa: ".black.bold, Itemdata);
-    const data = await Itemdata.save();
+    for(var i=0; i<20; i++){
+      const JsonData = {
+        Customer: {
+          id: uuidv4(),
+          firstname: faker.name.firstName(),
+          lastname: faker.name.lastName(),
+          avatarUrl: faker.image.avatar(),
+          email: faker.internet.email(),
+          phone: faker.phone.phoneNumber(),
+          hasPremium: true,
+          bids: [
+            Math.floor(Math.random() * 100) + 1,
+            Math.floor(Math.random() * 100) + 1,
+            Math.floor(Math.random() * 100) + 1,
+          ],
+        },
+        Bid: {
+          id: uuidv4(),
+          carTitle: carName.random(),
+          amount: faker.finance.amount(),
+        },
+      };
+
+      // console.log("jsonData: ", JsonData);
+
+      const Itemdata = new item(JsonData);
+      const data = await Itemdata.save();
+
+      if(data) console.log("data pushed")
+      else console.log("data error")
+    }
+    
+    // const Itemdata = new item(req.body);
+    // console.log("Daa: ".black.bold, Itemdata);
+    // const data = await Itemdata.save();
     res.status(200).send({
       success: true,
-      Data: data,
+      Data: "JsonData"
     });
   } catch (e) {
     res.status(400).send({
